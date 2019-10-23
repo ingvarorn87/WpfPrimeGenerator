66ce510873ea90d1572b1c6393ef4586c8193b7b
ingvarorn87 <ingvarorn87@gmail.com>
2019-10-23 21:05:30 +0200

    Added a console application
    Still need to create the GUI for that, now it´s operated through console with a switch statement

---
+++ ConsoleApplication/Primes/PrimeGenerator.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Primes
+{
+    class PrimeGenerator
+    {
+        public List<long> GetPrimesSequential(long first, long last)
+        {
+            List<long> primes = new List<long>();
+            List<long> result = new List<long>();
+            bool quit = false;
+            if (last >= 2) primes.Add(2);
+            if (2 <= last && 2 >= first) result.Add(2);
+            long currentNumber = 3;
+
+            while (!quit)
+            {
+                if (Console.KeyAvailable)
+                    quit = Console.ReadKey().Key == ConsoleKey.Escape;
+
+                if (CheckIfPrimeSequential(currentNumber, primes))
+                {
+                    primes.Add(currentNumber);
+                    if (currentNumber <= last && currentNumber >= first) result.Add(currentNumber);
+                }
+                if (currentNumber < last)
+                    currentNumber += 2;
+                else
+                    break;
+            }
+
+            return result;
+        }
+
+
+        public List<long> GetPrimesParallel(long first, long last)
+        {
+            List<long> primes = new List<long>();
+            List<long> result = new List<long>();
+
+            bool quit = false;
+            if (last >= 2) primes.Add(2);
+            if (2 <= last && 2 >= first) result.Add(2);
+            long currentNumber = 3;
+
+            while (!quit)
+            {
+                if (Console.KeyAvailable)
+                    quit = Console.ReadKey().Key == ConsoleKey.Escape;
+
+                if (CheckIfPrimeParallel(currentNumber, primes))
+                {
+                    primes.Add(currentNumber);
+                    if (currentNumber <= last && currentNumber >= first) result.Add(currentNumber);
+                }
+
+                if (currentNumber < last)
+                    currentNumber += 2;
+                else
+                    break;
+            }
+
+            return result;
+        }
+        private bool CheckIfPrimeParallel(long num, List<long> primes)
+        {
+            if ((num % 2) == 0) return false;
+
+            bool isPrimeNumber = true;
+
+            Parallel.ForEach(primes.TakeWhile(prime => prime * prime <= num),
+                (prime, loopState) =>
+                {
+                    if ((num % prime) == 0)
+                    {
+                        isPrimeNumber = false;
+                        loopState.Stop();
+                    }
+                });
+
+            return isPrimeNumber;
+        }
+        private bool CheckIfPrimeSequential(long num, List<long> primes)
+        {
+            if ((num % 2) == 0) return false;
+
+            foreach (long prime in primes.TakeWhile(prime => prime * prime <= num))
+            {
+                if ((num % prime) == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+    }
+}
---
+++ ConsoleApplication/Primes/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Primes
+{
+    class Program
+    {
+
+     
+        public static void Main(string[] args)
+        {
+            bool showMenu = true;
+            while (showMenu)
+            {
+                showMenu = MainMenu();
+            }
+        }
+
+        public static bool MainMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Welcome to the Prime Number Generator!");
+            Console.WriteLine("Please choose an option:");
+            Console.WriteLine("1) Select your own numbers");
+            Console.WriteLine("2) Find primes from 1 to 1.000.000");
+            Console.WriteLine("3) Exit the program");
+            Console.Write("Select an option: ");
+
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    EnterNumbers();
+                    return true;
+                case "2":
+                    PresetNumbers();
+                    return true;
+                case "3":
+                    return false;
+                default:
+                    return true;
+            }
+        }
+
+        public static void PresetNumbers()
+        {
+            long first = 1;
+            long last = 1000000;
+
+            PrimeGenerator primegenerator = new PrimeGenerator();
+            List<long> primesSequential = new List<long>();
+            List<long> primesParallel = new List<long>();
+
+            Console.WriteLine("Sequential calculation is running...");
+            Console.WriteLine("To stop the operation, press Esc");
+            MessureTime(() =>
+            {
+                primesSequential = primegenerator.GetPrimesSequential(first, last);
+            });
+
+            PrintInfo(primesSequential);
+
+            Console.WriteLine("Parallel calculation is running...");
+            Console.WriteLine("To stop the operation, press Esc");
+            MessureTime(() =>
+            {
+                primesParallel = primegenerator.GetPrimesParallel(first, last);
+            });
+
+            PrintInfo(primesParallel);
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey();
+        }
+
+        public static void EnterNumbers()
+        {
+            long first = 1;
+            long last = 0;
+
+            Console.Write("    Enter first number: ");
+            first = long.Parse(Console.ReadLine());
+            Console.Write("    Enter last number: ");
+            last = long.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            PrimeGenerator primegenerator = new PrimeGenerator();
+            List<long> primesSequantial = new List<long>();
+            List<long> primesParallel = new List<long>();
+
+            Console.WriteLine("Sequential calculation is running...");
+            Console.WriteLine("To stop the operation, press Esc");
+            MessureTime(() =>
+            {
+                primesSequantial = primegenerator.GetPrimesSequential(first, last);
+            });
+
+            PrintInfo(primesSequantial);
+
+            Console.WriteLine("Parallel calculation is running...");
+            Console.WriteLine("To stop the operation, press Esc");
+            MessureTime(() =>
+            {
+                primesParallel = primegenerator.GetPrimesParallel(first, last);
+            });
+
+            PrintInfo(primesParallel);
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey();
+        }
+
+        public static void MessureTime(Action p)
+        {
+            Stopwatch sw = Stopwatch.StartNew();
+            p.Invoke();
+            sw.Stop();
+            Console.WriteLine(" ------>The total time = {0:F5} sec.", sw.ElapsedMilliseconds / 1000d);
+        }
+        public static void PrintInfo(List<long> primes)
+        {
+            Console.WriteLine($"        Total prime numbers found: {primes.Count}");
+            Console.WriteLine($"        The smallest prime number found: {primes[0]}");
+            Console.WriteLine($"        The largest prime number found: {primes[primes.Count - 1]}");
+            Console.WriteLine();
+        }
+
+    }
+}
