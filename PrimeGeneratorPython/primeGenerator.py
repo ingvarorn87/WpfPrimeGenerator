fcfd7a26c9ee803121245a64de34aab9decdac2a
edijavi <edilata@msn.com>
2019-11-19 07:19:41 +0100

    Paralell part

--- PrimeGeneratorPython/primeGenerator.py
+++ PrimeGeneratorPython/primeGenerator.py
@@ -1,36 +1,52 @@
+from timeit import default_timer as timer
+from multiprocessing import Pool
+
+# Use an internal function to check if n is Prime
+def prime_numer_check(num):
+        if num <= 1:
+                return False
+  
+        for i in range(2,num):
+                if num % i == 0:
+                        return False
+
+        return num
 
 
 # Generator of prime numbers
-def prime_generator(init,final):
-        # Use an internal function to check if n is Prime
-        def isPrime(n):
-                if n <= 1:
-                        return False
-                
-                for i in range(2,n):
-                        if n % i == 0:
-                                return False
-                return True
+def secuential_prime_numbers(start_num,final_num):
+        start_time = timer()
+        primeList = []
+        while(start_num <= final_num):
+                if prime_numer_check(start_num):
+                        primeList.append(start_num)
+                start_num += 1   
+        elapsed_time = timer() - start_time
+        return '\nSecuential Method:\n', primeList, '\nElapsed Time:', elapsed_time
 
-        n = init
-        while(n <= final):
-                if isPrime(n):
-                        yield n
-                n += 1
+
+# Generator of prime numbers
+def parallel_prime_numbers(start_num, final_num):
+        start_time = timer()
+        p = Pool()
+        results = p.map(prime_numer_check, range(start_num, final_num+1))
+        results = list (filter(False.__ne__,results))  
+        p.close()
+        p.join()
+        elapsed_time = timer() - start_time
+        return '\nParallel Method:\n', results, '\nElapsed Time:', elapsed_time
+
 
 
 # Use the generator to iterate through the prime numbers:
 def main():
-    
-    init = int(input('\n\nEnter Initial Range Number: '))
-    final = int(input('\n\nEnter Final Range Number: '))
-
-    for n in prime_generator(init, final):
-            print (n,end=" ")
-    input('\n\nPress Enter to... ')
-    print() 
+        i_num = int(input('\nEnter Initial Range Number: '))
+        f_num = int(input('\nEnter Final Range Number: '))    
+        print('\n', *secuential_prime_numbers(i_num, f_num), '\n', *parallel_prime_numbers(i_num, f_num))
+        input('\n\nPress Enter to... ')
+        print() 
 
 
 # Run the Program
-
-main()
+if __name__ == "__main__":
+        main()
